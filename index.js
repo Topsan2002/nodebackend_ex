fba631d1586797842c61474f743fbc049712c6ac
Your Name <you@example.com>
2024-03-13 21:32:05 +0700

    .

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+// app.js or index.js
+require('dotenv').config();
 
 const books = require('./router/booking');
 const users = require('./router/users');
@@ -14,5 +16,7 @@ app.use('/RoomType',roomType)
 app.use('/Room',rooms)
 app.use('/Admin',admin)
 // Start the server
+
+console.log(process.env.TEST);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
